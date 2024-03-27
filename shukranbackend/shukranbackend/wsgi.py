c29673d53736569c8acbddd72853871c43caa27f
kimash-255 <rexviscot44@gmail.com>
2024-03-27 14:49:54 +0300

    Initial commit

---
+++ shukranbackend/shukranbackend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shukranbackend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shukranbackend.settings')
+
+application = get_wsgi_application()
