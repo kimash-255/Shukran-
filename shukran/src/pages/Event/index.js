d3842bb2b1ee68d60264a3cd5951bc3c20a6b550
kimash-255 <rexviscot44@gmail.com>
2024-03-27 18:29:03 +0300

    events1

--- shukran/src/pages/Event/index.js
+++ shukran/src/pages/Event/index.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
-import Hero from "@/components/hero";
+
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Events from "@/components/events";
-import Layout from "./Layout";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
